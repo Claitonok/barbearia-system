08f1b52508c2ad2fbd9c49e65199e793bd716655
Claitonok <claiton_santos1@hotmail.com>
2026-05-10 18:59:24 -0300

    API - Online

--- api/auth/route.ts
+++ api/auth/route.ts
@@ -1,7 +1,7 @@
 import { AgendamentoHorario, ResponseAgendamentoHorario, UsuarioAdmin } from "@/types/dados";
 
 //const API_URL = "http://localhost:8080/barbearia/json"; // My Localhost
-const API_URL = "https://system-barbearia-production.up.railway.app"; // Railway Deployment
+const API_URL = "https://system-barbearia-production.up.railway.app/"; // Railway Deployment
 
 // Recriando a credencial de forma segura
 const auth = Buffer.from(`${process.env.NEXT_PUBLIC_SYSTEM_USER}:${process.env.NEXT_PUBLIC_SYSTEM_PASS}`).toString('base64');
