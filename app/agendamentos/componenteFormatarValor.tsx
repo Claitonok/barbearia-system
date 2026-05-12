ab3c3642a23914c2cdc504a3b58553700f18c3e0
Claitonok <claiton_santos1@hotmail.com>
2026-05-12 10:39:58 -0300

    Serviços ajuste+++

--- app/agendamentos/componenteFormatarValor.tsx
+++ app/agendamentos/componenteFormatarValor.tsx
@@ -7,17 +7,21 @@ interface formValor {
 
 export default function ComponenteFormatarValor(resp: formValor) {
 
-    if (resp.valor == 45) {
+    if (resp.valor == 60) {
         return <>
-            <input type="text" name="txtModelo" placeholder={"Corte | barba: " + formatarMoeda(resp.valor)} id="" />
+            <input type="text" name="txtModelo" placeholder={"Combo: Corte | Barba | Sobrancelha: " + formatarMoeda(resp.valor)} id="" />
         </>
     } else if (resp.valor == 35) {
         return <>
-           <input type="text" name="txtModelo" placeholder={"Corte: " + formatarMoeda(resp.valor)} id="" />
+           <input type="text" name="txtModelo" placeholder={"Corte tradicional: " + formatarMoeda(resp.valor)} id="" />
         </>
-    } else if (resp.valor == 10) {
+    } else if (resp.valor == 15) {
         return <>
-            <input type="text" name="txtModelo" placeholder={"barba: " + formatarMoeda(resp.valor)} id="" />
+            <input type="text" name="txtModelo" placeholder={"Barba Completa: " + formatarMoeda(resp.valor)} id="" />
+        </>
+    } else if (resp.valor == 10){
+           return <>
+            <input type="text" name="txtModelo" placeholder={"Sobrancelha: " + formatarMoeda(resp.valor)} id="" />
         </>
     }
 
